bfeba1d2ac2417f701bdf74ed88e62ed3f6d9ed4
Dave Poole <davethegr8@gmail.com>
2016-03-22 21:46:34 -0700

    add preview of staff for homepage

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var imageResize = require('gulp-image-resize');
+
+gulp.task('default', function () {
+  gulp.src('assets/**/*.jpg')
+    .pipe(imageResize({
+      width : 300,
+      height : 300
+    }))
+    .pipe(gulp.dest('website/public/assets'));
+});
