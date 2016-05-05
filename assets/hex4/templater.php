c88b20b773cdbb8edfe7476346a007d1cd341bfd
David Poole <david.poole@dpoolseaosxl2.bigfish.lan>
2016-05-05 14:30:50 -0700

    add tests, formidable info

---
+++ assets/hex4/templater.php
@@ -0,0 +1,309 @@
+<?php
+
+$colors = [];
+
+// raw
+// $colors = array_merge($colors, [
+//   'green' => '#04ff00',
+//   'blue' => '#005eff',
+//   'red' => '#ff002b',
+//   'purple' => '#8000ff',
+//   'orange' => '#ff7700'
+// ]);
+
+// primary
+/*$colors = array_merge($colors, [
+  'green' => '#022d01',
+  'blue' => '#1c2d4a',
+  'red' => '#99001a',
+  'purple' => '#663399',
+  'orange' => '#ed7e1d'
+]);*/
+
+// secondary
+// $colors = array_merge($colors, [
+//   'green' => '#137630',
+//   'blue' => '#014da6',
+//   'red' => '#ff2a1c',
+//   'purple' => '#9c2172',
+//   'orange' => '#f35221'
+// ]);
+
+// neutral
+// $colors = array_merge($colors, [
+//   'green' => '#31583c',
+//   'blue' => '#335175',
+//   'red' => '#bb6660',
+//   'purple' => '#774666',
+//   'orange' => '#b47460'
+// ]);
+
+//
+// $colors = array_merge($colors, [
+//   'mostly-white' => '#e2efe6',
+//   'mostly-black' => '#171717'
+// ]);
+
+
+$polygons = [
+    0 => [
+        "points" => "900,0 900,300 640,150",
+        "colors" => ["#9933FF", "#583AAD"]
+    ],
+    1 => [
+        "points" => "900,0 900,300 1160,150",
+        "colors" => ["#9933FF"]
+    ],
+    2 => [
+        "points" => "900,300 640,450 900,600",
+        "colors" => ["#583AAD"]
+    ],
+    3 => [
+        "points" => "900,300 1160,450 900,600",
+        "colors" => ["#763B94"]
+    ],
+    4 => [
+        "points" => "900,900 900,600 640,750",
+        "colors" => ["#AB6B67"]
+    ],
+    5 => [
+        "points" => "900,900 900,1200 640,1050",
+        "colors" => ["#BD7257"]
+    ],
+    6 => [
+        "points" => "900,1500 900,1200 640,1350",
+        "colors" => ["#7F3178"]
+    ],
+    7 => [
+        "points" => "900,1500 900,1800 640,1650",
+        "colors" => ["#3471B8"]
+    ],
+    8 => [
+        "points" => "900,900 900,600 1160,750",
+        "colors" => ["#AA6A68"]
+    ],
+    9 => [
+        "points" => "900,900 900,1200 1160,1050",
+        "colors" => ["#AF6664"]
+    ],
+    10 => [
+        "points" => "900,1500 900,1200 1160,1350",
+        "colors" => ["#872C75"]
+    ],
+    11 => [
+        "points" => "900,1500 900,1800 1160,1650",
+        "colors" => ["#3471B8"]
+    ],
+    12 => [
+        "points" => "640,150 900,300 640,450",
+        "colors" => ["#1F55FF"]
+    ],
+    13 => [
+        "points" => "640,450 900,600 640,750",
+        "colors" => ["#7C4185"]
+    ],
+    14 => [
+        "points" => "640,750 900,900 640,1050",
+        "colors" => ["#C27650"]
+    ],
+    15 => [
+        "points" => "640,1050 900,1200 640,1350",
+        "colors" => ["#D96B2B"]
+    ],
+    16 => [
+        "points" => "640,1350 900,1500 640,1650",
+        "colors" => ["#0C718F"]
+    ],
+    17 => [
+        "points" => "640,150 380,300 640,450",
+        "colors" => ["#337EFF"]
+    ],
+    18 => [
+        "points" => "640,450 380,600 640,750",
+        "colors" => ["#D67214"]
+    ],
+    19 => [
+        "points" => "640,750 380,900 640,1050",
+        "colors" => ["#F57708"]
+    ],
+    20 => [
+        "points" => "640,1050 380,1200 640,1350",
+        "colors" => ["#C08819"]
+    ],
+    21 => [
+        "points" => "640,1350 380,1500 640,1650",
+        "colors" => ["#36FF33"]
+    ],
+    22 => [
+        "points" => "640,450 380,600 380,300",
+        "colors" => ["#316063"]
+    ],
+    23 => [
+        "points" => "640,750 380,900 380,600",
+        "colors" => ["#D3770C"]
+    ],
+    24 => [
+        "points" => "640,1050 380,1200 380,900",
+        "colors" => ["#C1691B"]
+    ],
+    25 => [
+        "points" => "640,1350 380,1500 380,1200",
+        "colors" => ["#0EDD0A"]
+    ],
+    26 => [
+        "points" => "120,450 380,600 380,300",
+        "colors" => ["#5A7963"]
+    ],
+    27 => [
+        "points" => "120,750 380,900 380,600",
+        "colors" => ["#21773A"]
+    ],
+    28 => [
+        "points" => "120,1050 380,1200 380,900",
+        "colors" => ["#0F4113"]
+    ],
+    29 => [
+        "points" => "120,1350 380,1500 380,1200",
+        "colors" => ["#355734"]
+    ],
+    30 => [
+        "points" => "120,750 120,450 380,600",
+        "colors" => ["#5A7963"]
+    ],
+    31 => [
+        "points" => "120,1050 120,750 380,900",
+        "colors" => ["#429159"]
+    ],
+    32 => [
+        "points" => "120,1350 120,1050 380,1200",
+        "colors" => ["#355734"]
+    ],
+    33 => [
+        "points" => "900,300 1160,150 1160,450",
+        "colors" => ["#B45C80"]
+    ],
+    34 => [
+        "points" => "900,600 1160,750 1160,450",
+        "colors" => ["#813B85"]
+    ],
+    35 => [
+        "points" => "900,900 1160,750 1160,1050",
+        "colors" => ["#AD6D60"]
+    ],
+    36 => [
+        "points" => "900,1200 1160,1050 1160,1350",
+        "colors" => ["#942A71"]
+    ],
+    37 => [
+        "points" => "900,1500 1160,1350 1160,1650",
+        "colors" => ["#335782"]
+    ],
+    38 => [
+        "points" => "1420,300 1160,150 1160,450",
+        "colors" => ["#C98580"]
+    ],
+    39 => [
+        "points" => "1420,600 1160,750 1160,450",
+        "colors" => ["#EB552D"]
+    ],
+    40 => [
+        "points" => "1420,900 1160,750 1160,1050",
+        "colors" => ["#8D475A"]
+    ],
+    41 => [
+        "points" => "1420,1200 1160,1050 1160,1350",
+        "colors" => ["#8C3D5E"]
+    ],
+    42 => [
+        "points" => "1420,1500 1160,1350 1160,1650",
+        "colors" => ["#5C7491"]
+    ],
+    43 => [
+        "points" => "1420,600 1420,300 1160,450",
+        "colors" => ["#C9635B"]
+    ],
+    44 => [
+        "points" => "1420,900 1160,750 1420,600",
+        "colors" => ["#F24A22"]
+    ],
+    45 => [
+        "points" => "1420,1200 1160,1050 1420,900",
+        "colors" => ["#823A59"]
+    ],
+    46 => [
+        "points" => "1420,1500 1160,1350 1420,1200",
+        "colors" => ["#DE173F"]
+    ],
+    47 => [
+        "points" => "1420,600 1420,300 1680,450",
+        "colors" => ["#FF5549"]
+    ],
+    48 => [
+        "points" => "1420,900 1420,600 1680,750",
+        "colors" => ["#EF2C25"]
+    ],
+    49 => [
+        "points" => "1420,1200 1420,900 1680,1050",
+        "colors" => ["#AD0A26"]
+    ],
+    50 => [
+        "points" => "1420,1500 1420,1200 1680,1350",
+        "colors" => ["#FF3355"]
+    ],
+    51 => [
+        "points" => "1680,450 1420,600 1680,750",
+        "colors" => ["#FF5549"]
+    ],
+    52 => [
+        "points" => "1680,750 1420,900 1680,1050",
+        "colors" => ["#AD3348"]
+    ],
+    53 => [
+        "points" => "1680,1050 1420,1200 1680,1350",
+        "colors" => ["#FF3355"]
+    ],
+];
+
+$polyTemplate = '<polygon id="poly-{index}" points="{points}" style="fill:{start};">{animate}</polygon>';
+$animateTemplate = '<animate attributeName="fill" values="{colors}" repeatCount="indefinite" begin="{begin}s" dur="{dur}s" />';
+
+$templateData = [
+    'polygons' => []
+];
+
+foreach($polygons as $index => $item) {
+    $item['start'] = $item['colors'][0];
+    $item['index'] = $index;
+    $item['animate'] = '';
+
+    if(count($item['colors']) > 1) {
+        $item['colors'][] = $item['colors'][0];
+        $item['colors'] = implode('; ', $item['colors']);
+        $item['begin'] = rand(0,2);
+        $item['dur'] = rand(3,6);
+        $item['animate'] = interpolate($animateTemplate, $item);
+    }
+
+    $templateData['polygons'][] = interpolate($polyTemplate, $item);
+}
+
+$templateData['polygons'] = implode("\n", $templateData['polygons']);
+
+$svg = file_get_contents('template.svg');
+$svg = interpolate($svg, $templateData);
+file_put_contents('test.svg', $svg);
+
+/**
+* Interpolates context values into the message placeholders.
+*/
+function interpolate($message, array $context = array())
+{
+    // build a replacement array with braces around the context keys
+    $replace = array();
+    foreach ($context as $key => $val) {
+        $replace['{' . $key . '}'] = $val;
+    }
+
+    // interpolate replacement values into the message and return
+    return strtr($message, $replace);
+}
